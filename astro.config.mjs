992dc6520d33137bf3897908f9652f2e19a3b13b
Robert Abreu <robert.abreu3@gmail.com>
2024-04-03 22:25:02 -0400

    config astro for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bio.fiveeightyeight.github.io',
+  base: 'bio'.
   integrations: [tailwind()]
 });
