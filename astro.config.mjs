36197864154cb3bb24eef0cb2e592b501b356372
Robert Abreu <robert.abreu3@gmail.com>
2024-04-03 22:29:26 -0400

    fix config typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bio.fiveeightyeight.github.io',
-  base: 'bio'.
+  base: 'bio',
   integrations: [tailwind()]
 });
