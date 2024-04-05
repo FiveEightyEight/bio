a6629ba430b98a0009b03c31a96f7d28e997a43a
Robert Abreu <robert.abreu3@gmail.com>
2024-04-05 12:29:55 -0400

    adjust styling to left, move animation to tw

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				'loader-spin': 'spin 2s linear infinite',
+			},
 			fontFamily: {
 				sans: ['Geist Sans', ...defaultTheme.fontFamily.sans],
+			},
+			keyframes: {
+				spin: {
+					from: { transform: 'rotate(0deg)' },
+					to: { transform: 'rotate(360deg)' },
+				},
 			},
 		},
 	},
