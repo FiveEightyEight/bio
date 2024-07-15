9f0e7a4448f5cf0fc8c64b07b13e478a479cca1c
FiveEightyEight <robert.abreu3@gmail.com>
2024-07-15 19:39:01 -0400

    install astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import playformCompress from "@playform/compress";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rabreu.dev',
   base: '',
-  integrations: [tailwind(), playformCompress()]
+  integrations: [tailwind(), playformCompress(), icon()]
 });
