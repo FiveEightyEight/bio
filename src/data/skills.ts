4890ee8649eb39bc5218c1d1b137a659031fd04d
FiveEightyEight <robert.abreu3@gmail.com>
2024-07-18 19:24:10 -0400

    add skill data

---
+++ src/data/skills.ts
@@ -0,0 +1,52 @@
+type Skill = {
+    icon: string;
+    label: string;
+};
+const skills: Skill[] = [
+    {
+        label: "React",
+        icon: "react"
+    },
+    {
+        label: "Node",
+        icon: "node"
+    },
+    {
+        label: "TypeScript",
+        icon: "typescript"
+    },
+    {
+        label: "React Native",
+        icon: "react"
+    },
+    {
+        label: "JavaScript",
+        icon: "javascript"
+    },
+    {
+        label: "GraphQL",
+        icon: "graphql"
+    },
+    {
+        label: "Python",
+        icon: "python"
+    },
+    {
+        label: "CSS",
+        icon: "css"
+    },
+    {
+        label: "Git",
+        icon: "git"
+    },
+    {
+        label: "SQL",
+        icon: "db"
+    },
+    {
+        label: "AWS",
+        icon: "aws"
+    },
+];
+
+export default skills;
